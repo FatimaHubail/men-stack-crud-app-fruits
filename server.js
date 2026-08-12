613898a29e4f3fd6c83dd595a087296c9e950660
FatimaHubail <fhubail03@gmail.com>
2026-08-12 14:57:39 +0300

    add method-override middleware and implement fruit edit and delete functionality with corresponding views

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Fruit = require('./models/fruit.js');
 const morgan = require('morgan');
+const override = require('method-override');
 
 dotenv.config();
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(override("_method"))
 app.use(morgan('dev'));
 
 // DB connection
@@ -42,8 +44,9 @@ app.post('/fruits', async (req, res) => {
     } catch (error) {
         console.log('error');
     }
-})
+});
 
+// rendering all fruits
 app.get('/fruits', async (req, res) => {
     try {
         const allFruits = await Fruit.find();
@@ -51,8 +54,52 @@ app.get('/fruits', async (req, res) => {
     } catch (error) {
         res.send('failed to get all fruits');
     }
+});
+
+// show single fruit info
+app.get('/fruits/:id', async (req, res) => {
+    try {
+        const fruit = await Fruit.findById(req.params.id);
+        res.render("fruits/show.ejs", { fruit });
+    } catch (error) {
+        console.log('failed to fetch the fruit');
+    }
 })
 
+// delete fruit
+app.delete('/fruits/:id', async (req, res) => {
+    try {
+        await Fruit.findByIdAndDelete(req.params.id);
+        res.redirect("/fruits");
+    } catch (error) {
+        res.send('unable to delete fruit');
+    }
+})
+
+// edit fruit
+app.get('/fruits/:id/edit', async (req, res) => {
+    try {
+        const fruit = await Fruit.findById(req.params.id);
+        res.render('fruits/edit.ejs', { fruit });
+    } catch (error) {
+        res.send('unable to update the fruit');
+    }
+})
+
+app.put('/fruits/:id', async (req, res) => {
+    try {
+        if (req.body.isReadyToEat === "on") {
+            req.body.isReadyToEat = true;
+        } else {
+            req.body.isReadyToEat = false;
+        }
+        await Fruit.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect(`/fruits/${req.params.id}`); 
+    } catch (error) {
+        console.log(error);
+        res.send('Coun')
+    }
+})
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
